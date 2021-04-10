ba49120087f616f01798c230eda436641b717a37
Joel Adie <adiejoel14+monarch@gmail.com>
2021-04-10 09:03:36 +0100

    Created serviceData component.
    Moved all of the Data in services to the serviceData component I created, to be able to programmably map through them and render their values.

--- components/Services.jsx
+++ components/Services.jsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import styles from "@/components/services.module.css";
 // import Image from "next/image";
 // import Skeleton from "react-loading-skeleton";
+import Data from "./serviceData";
 
 const isServer = typeof window === "undefined";
 const WOW = !isServer ? require("wow.js") : null;
@@ -18,90 +19,24 @@ const Services = () => {
 				Our Services
 			</h3>
 			<div className={styles.row}>
-				<div className={`${styles.card} ${"wow flipInY"}`}>
-					<div className={styles.content}>
-						<h2 className={styles.h2}>01</h2>
-						<h3 className={styles.h3}>Web Development</h3>
-						<p className={styles.p}>
-							Our Services range from developing a simple portfolio page to complex web applications, electronic businesses,
-							web scraping, with scalable and reliable backend
-							services using modern web technologies.
-						</p>
+				{Data.map((singleData, index) => (
+					<div className={`${styles.card} ${"wow flipInY"}`}>
+						<div className={styles.content}>
+							<h2 className={styles.h2} key={singleData.h2 + index}>
+								{singleData.h2}
+							</h2>
+							<h3 className={styles.h3} key={singleData.h3 + index}>
+								{singleData.h3}
+							</h3>
+							<p className={styles.p} key={singleData.p + index}>
+								{singleData.p}
+							</p>
+						</div>
+						<Link href='/#contact'>
+							<a className={styles.a}>Get a quote</a>
+						</Link>
 					</div>
-					<Link href='/#contact'>
-						<a className={styles.a}>Get a quote</a>
-					</Link>
-				</div>
-				<div className={`${styles.card} ${"wow flipInY"}`}>
-					<div className={styles.content}>
-						<h2 className={styles.h2}>02</h2>
-						<h3 className={styles.h3}>Mobile App Development</h3>
-						<p className={styles.p}>
-							From development to deployment, we build robust Android and iOS
-							apps, and where required, publish to Google Play Store and Apple
-							Store respectively.
-						</p>
-					</div>
-					<Link href='/#contact'>
-						<a className={styles.a}>Get a quote</a>
-					</Link>
-				</div>
-				<div className={`${styles.card} ${"wow flipInY"}`}>
-					<div className={styles.content}>
-						<h2 className={styles.h2}>03</h2>
-						<h3 className={styles.h3}>Graphics Design</h3>
-						<p className={styles.p}>
-							By applying visual hierarchy and page layout techniques, we craft
-							professional visual content, interactive designs that communicates
-							instantly with clients. Logos, banners etc
-						</p>
-					</div>
-					<Link href='/#contact'>
-						<a className={styles.a}>Get a quote</a>
-					</Link>
-				</div>
-				<div className={`${styles.card} ${"wow flipInY"}`}>
-					<div className={styles.content}>
-						<h2 className={styles.h2}>04</h2>
-						<h3 className={styles.h3}>UI & UX Design</h3>
-						<p className={styles.p}>
-							You can count on us to deliver beautiful User Interfaces and
-							experience as well as mockups and prototypes for your next
-							project.
-						</p>
-					</div>
-					<Link href='/#contact'>
-						<a className={styles.a}>Get a quote</a>
-					</Link>
-				</div>
-				<div className={`${styles.card} ${"wow flipInY"}`}>
-					<div className={styles.content}>
-						<h2 className={styles.h2}>05</h2>
-						<h3 className={styles.h3}>Tutoring & Career Counselling</h3>
-						<p className={styles.p}>
-							Get high standard career advice and training from our coaches in
-							the different technology stacks, ranging from web development to
-							blockchain development.
-						</p>
-					</div>
-					<Link href='/#contact'>
-						<a className={styles.a}>Get a quote</a>
-					</Link>
-				</div>
-				<div className={`${styles.card} ${"wow flipInY"}`}>
-					<div className={styles.content}>
-						<h2 className={styles.h2}>06</h2>
-						<h3 className={styles.h3}>Shopify Themes & Applications</h3>
-						<p className={styles.p}>
-							Turn your retail store into an online store in the world's largest
-							online store. we build custom shopify themes and applications that
-							will suit your needs.
-						</p>
-					</div>
-					<Link href='/#contact'>
-						<a className={styles.a}>Get a quote</a>
-					</Link>
-				</div>
+				))}
 			</div>
 		</div>
 	);
---
+++ components/serviceData.jsx
@@ -0,0 +1,39 @@
+const Data = [
+	{
+		h2: "01",
+		h3: "Web Development",
+		p:
+			"Our Services range from developing a simple portfolio page to complex web applications, electronic businesses, web scraping, with scalable and reliable backend services using modern web technologies."
+	},
+	{
+		h2: "02",
+		h3: "Mobile App Development",
+		p:
+			"From development to deployment, we build robust Android and iOS apps, and where required, publish to Google Play Store and Apple Store respectively."
+	},
+	{
+		h2: "03",
+		h3: "Graphics Design",
+		p:
+			"By applying visual hierarchy and page layout techniques, we craft professional visual content, interactive designs that communicates instantly with clients. Logos, banners etc."
+	},
+	{
+		h2: "04",
+		h3: "UI & UX Design",
+		p:
+			"You can count on us to deliver beautiful User Interfaces and experience as well as mockups and prototypes for your next project."
+	},
+	{
+		h2: "05",
+		h3: "Tutoring & Career Counselling",
+		p:
+			"Get high standard career advice and training from our coaches in the different technology stacks, ranging from web development to blockchain development."
+    },
+    {
+        h2: "06",
+        h3: "Shopify Themes & Applications",
+        p:    "Turn your retail store into an online store in the world's largest online store. we build custom shopify themes and applications that will suit your needs."
+    }
+]; 
+
+export default Data
