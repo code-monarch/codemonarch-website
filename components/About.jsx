eedb90d7829572667cac3f9e4d3f7208f2fe921c
Joel Adie <adiejoel14+monarch@gmail.com>
2021-04-11 07:03:21 +0100

    Edit About.jsx

--- components/About.jsx
+++ components/About.jsx
@@ -17,25 +17,29 @@ const About = ({toggleModal}) => {
 		<div id='about' className={styles.container}>
 			<div className={`${styles.AboutTitle} ${"wow fadeInUpBig"}`}>
 				<h3 className={`${styles.h3} ${"wow fadeInUpBig"}`}>About us</h3>
-				<p className={`${styles.p} ${"wow fadeInUpBig"}`} data-wow-delay='30ms'>
-					We're a team of experienced developers and designers skilled in
-					building / designing proven software solutions that boosts businesses
-					and promote organizational sustainability. We help in taking
-					<b> ideas </b>
-					from conception to <b> production </b>and improve sales by engaging
-					more clients in your space.
-					<br />
-					Join <b>hundreds</b> of businesses who are doing more with technology.
-					Get the product / service you desire <b>within your budget.</b>
-					<br />
-				</p>
+				<div
+					className={`${styles.textWrapper} ${"wow fadeInUpBig"}`}
+					data-wow-delay='40ms'>
+					<p
+						className={styles.p}>
+						We're a team of experienced developers and designers skilled in
+						building / designing proven software solutions that boosts
+						businesses and promote organizational sustainability. We help in
+						taking
+						<b> ideas </b>
+						from conception to <b> production </b>and improve sales by engaging
+						more clients in your space.
+						<br />
+						Join <b>hundreds</b> of businesses who are doing more with
+						technology. Get the product / service you desire{" "}
+						<b>within your budget.</b>
+						<br />
+					</p>
 
-				<span
-					className={`${styles.consult} ${"wow fadeInUpBig"}`}
-					data-wow-delay='0ms'
-					onClick={toggleModal}>
-					Get free consultation
-				</span>
+					<span className={styles.consult} onClick={toggleModal}>
+						Get free consultation
+					</span>
+				</div>
 			</div>
 			{/* <Modal toggleModal={toggleModal} /> */}
 		</div>
